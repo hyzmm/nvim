afed9cead7a4c961d4bbd3495a7406779c72594a
binary <binary.weng@idreamsky.com>
2024-03-01 19:39:45 +0800

    wip

--- lua/base.lua
+++ lua/base.lua
@@ -14,6 +14,7 @@ g.neovide_theme = 'dark'
 g.neovide_remember_window_size = true
 g.neovide_cursor_animation_length = 0.13
 g.neovide_cursor_animate_command_line = true
+g.neovide_input_macos_alt_is_meta = true
 
 g.neovide_cursor_vfx_mode = "railgun"
 g.neovide_cursor_vfx_particle_density = 10.0
